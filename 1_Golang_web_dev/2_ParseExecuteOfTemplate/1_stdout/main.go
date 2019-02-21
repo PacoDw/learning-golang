a4c5d442310b16942b09dd5bde8221565e208da7
Paco Preciado Mendoza <__pm@outlook.com>
2019-02-20 22:15:03 -0600

    :ok_hand: Parsing files with text/template library was added

--- 1_Golang_web_dev/2_ParseExecuteOfTemplate/1_stdout/main.go
+++ 1_Golang_web_dev/2_ParseExecuteOfTemplate/1_stdout/main.go
@@ -10,12 +10,12 @@ func main() {
 	/*Template is a container that contains all the html files (whatever it is) inside itself.
 	 * ParseFiles: get n names of files to store them returning the *template (container)
 	 */
-	tpl, err := template.ParseFiles("1-file.gohtml", "1-file.gohtml")
+	tpl, err := template.ParseFiles("./1-file.gohtml", "./2-file.gohtml")
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	/*Execute parse all the templates and passes the content in a writer as the firs parameter
+	/*Execute parse ONLY the first file of the *template and passes the content in a writer as the first parameter
 	 *the parameter could be a fille for example
 	 */
 	err = tpl.Execute(os.Stdout, nil)
